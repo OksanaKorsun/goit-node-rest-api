188c3296eddb689f7981e5fbd678c75ed57b5f91
OksanaKorsun <oksana2081989@gmail.com>
2024-01-18 12:07:10 +0200

    first

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,18 @@
+const contactsService = require("../services/contactsServices.js");
+
+const getAllContacts = (req, res) => {};
+
+const getContactById = (req, res) => {};
+
+const deleteContact = (req, res) => {};
+
+const createContact = (req, res) => {};
+
+const updateContact = (req, res) => {};
+module.exports = {
+  getAllContacts,
+  getContactById,
+  deleteContact,
+  createContact,
+  updateContact,
+};
